1fdf564c5f08aeba8198d56ae5aad8598bba79b7
Ben <bwisler95@gmail.com>
2018-07-10 19:47:26 -0400

    intial repo setup

---
+++ src/articleSchema.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var articleSchema = new Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    url: {
+        type: String,
+         required: true,
+    }
+})
+
+var Article = mongoose.model("Article", articleSchema);
+
+module.exports = Article;
